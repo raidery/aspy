c1e29df1b6917e02579f2b0ce4d9eb065de6048e
Raider <raidery@gmail.com>
2017-11-02 04:20:57 -0500

    Update analytic_server.py

--- aspy/analytic_server.py
+++ aspy/analytic_server.py
@@ -1,17 +1,3 @@
-"""
-IBM Confidential
-
-OCO Source Materials
-
-IBM SPSS Products: Analytic Server
-
-(C) Copyright IBM Corp. 2016
-
-The source code for this program is not published or otherwise
-divested of its trade secrets, irrespective of what has been
-deposited with the U.S. Copyright Office.
-
-"""
 
 import httplib
 import base64
