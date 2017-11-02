6a3c1ecf98b0bb44b4fa1d38d1bc1d627efd9add
Raider <raidery@gmail.com>
2017-11-02 01:40:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+
+import os
+import sys
 from setuptools import setup
  
 setup(
      name='aspy',    # This is the name of your PyPI-package.
+     packages = ['aspy'],
      version='0.1',                          # Update the version number for new releases
 )
