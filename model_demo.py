a98738f5ca577a19c8c2632f90dedc9b87835ffe
Raidery <raidery@gmail.com>
2017-11-13 10:47:44 +0800

    commit files

--- model_demo.py
+++ model_demo.py
@@ -3,7 +3,7 @@ from aspy.aslgen import analytic_server
 
 logger = logging.getLogger('aspy')
 
-a = analytic_server("rhe701.fyre.ibm.com",9080,False,"admin","test")
+ass = analytic_server("rhe701.fyre.ibm.com",9080,False,"admin","test")
 
 
 
@@ -28,16 +28,16 @@ print(str(rs.toPandas()))
 """
 conf_tree = {"InputFieldList": ["Age","Sex","BP","Cholesterol"],"TargetField": "Drug","TreeGrowingMethod": "Chaid"}
 conf_outcon = [{"export": True,"name": "PMML","uri": "/pmml.con"},{"export": True,"name": "StatXML","uri": "/stat.con"}]
-modeling = a.create_pipeline().read_datasource("DRUG1N").select("lambda x: x.Age>10").derive("ratio","lambda x:x.Na/x.K").tree(conf_tree,conf_outcon).run()
+modeling = ass.create_pipeline().read_datasource("DRUG1N").select("lambda x: x.Age>10").derive("ratio","lambda x:x.Na/x.K").tree(conf_tree,conf_outcon).run()
 """
 
 
 """
 scoreConf = {"isOutputInputData": True}
 scoreInputConf = [{"export": True,"name": "InputPMML","uri": "/pmml.con"}]
-scoring = a.create_pipeline().read_datasource("DRUG1N").scoring(scoreConf,scoreInputConf).run()
+scoring = ass.create_pipeline().read_datasource("DRUG1N").scoring(scoreConf,scoreInputConf).run()
 """
 
 buildConf = {"InputFieldList": ["Sex","BP","Cholesterol"],"TargetField": "Age"}
 inputConf = [{"export": True,"name": "PMML","uri": "/pmml.con"},{"export": True,"name": "StatXML","uri": "/stat.con"}]
-modeling = a.create_pipeline().read_datasource("DRUG1N").select("lambda x: x.Age>10").linear(buildConf,inputConf).run()
+modeling = as.create_pipeline().read_datasource("DRUG1N").select("lambda x: x.Age>10").linear(buildConf,inputConf).run()
